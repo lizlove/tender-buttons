4934490e39f8d6413b137a72d4f442ca5c4d238a
lizlove <elizabethlovero@gmail.com>
2017-04-18 15:27:29 -0400

    feat(mandrill) stub out mandrill mailer

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const favicon = require('serve-favicon');
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,31 +4,91 @@ class Contact extends React.Component {
   constructor(props) {
     super(props);
 
-  }
-  handleMessageSubmit(){
+    this.state = {
+      name:'',
+      email: '',
+      message: '',
+      human: false,
+      robot: true,
+      errorMsg: ''
+    }
 
+    this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
+
+  }
+
+  handleMessageSubmit(){
+    const body = `Contact Form Submission from: ${this.state.name} at ${this.state.email}. They said: ${this.state.message}.`;
+    const username = process.env.MC_USER,
+    const password = process.env.MC_PASS;
+    console.log(body, username, password);
+    // Mailer.send(
+    //   { host:           "smtp.mandrillapp.com"
+    //   , port:           587
+    //   , to:             "elizabethlovero@gmail.com"
+    //   , from:           "hello@tenderbuttons.com"
+    //   , subject:        "[Tender Buttons] Contact Form Submission"
+    //   , body:           body
+    //   , authentication: "login"
+    //   , username:       username
+    //   , password:       password
+    //   }, function(err, result){
+    //     if(err){
+    //       console.log(err);
+    //     }
+    //   }
+    // );
+  }
+
+  handleStateChange(key, value) {
+    let newState = Object.assign(this.state, {key: value});
+    this.setState(newState);
   }
 
   render() {
+    let error = this.state.errorMsg ? <p>this.state.errorMsg</p> : null;
     return (
       <div>
           <h2>Contact Us</h2>
+          {error}
           <form method="post" action="#">
             <div className="field">
-              <input type="text" name="name" id="name" placeholder="Name" />
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={e => this.handleStateChange("name", e.target.value.trim())}
+              />
             </div>
             <div className="field">
-              <input type="email" name="email" id="email" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={e => this.handleStateChange("email", e.target.value.trim())}
+              />
             </div>
             <div className="field">
-              <textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
+              <textarea
+                name="message"
+                placeholder="Message"
+                rows="4"
+                onChange={e => this.handleStateChange("message", e.target.value.trim())}>
+              </textarea>
             </div>
             <div className="field">
-              <input type="checkbox" id="human" name="human" /><label>I'm a human</label>
+              <input
+                type="checkbox"
+                name="human"
+                checked={this.state.human}
+                onChange={e => this.handleStateChange("human", e.target.value)}
+              />
+                <label>I'm a human</label>
             </div>
             <div className="field">
               <label>But are you a robot?</label>
-              <input type="radio" id="robot_yes" name="robot" /><label>Yes</label>
+              <input type="radio" id="robot_yes" name="robot"/><label>Yes</label>
               <input type="radio" id="robot_no" name="robot" /><label>No</label>
             </div>
             <ul className="actions">
