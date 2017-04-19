e6fe7cb9a0119c68642140f9f699366adc6a75e2
lizlove <elizabethlovero@gmail.com>
2017-04-18 20:18:10 -0400

    fix(router): fix router to work on reload.

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const express = express();
-const morgan = require('morgan');
 const favicon = require('serve-favicon');
+const mailer   = require("mailer");
+require('dotenv').config()
 
 const app = express();
+var port = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/contactus', function (req, res) {
+  console.log(req.body);
+  // mailer.send(
+  //   { host:           "smtp.mandrillapp.com"
+  //   , port:           587
+  //   , to:             "customer@anydomain.com"
+  //   , from:           "you@yourdomain.com"
+  //   , subject:        "Mandrill knows Javascript!"
+  //   , body:           req.body
+  //   , authentication: "login"
+  //   , username:       process.env.MC_USER
+  //   , password:       process.env.MC_PASS
+  //   }, function(err, res){
+  //     if(err){
+  //       res.render('error', { error: err })
+  //     } else {
+  //       console.log(res);
+  //     }
+  //   }
+  // );
+});
 
 // Always return the main index.html, so react-router renders the route in the client
 app.get('*', (req, res) => {
-  res.use(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
 
 module.exports = app;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,50 @@
 'use strict';
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
 
     this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+  }
+
+  sendEmail(){
+    const postData = `Contact from: ${this.props.name}, at: ${this.props.email}. Message: ${this.props.message}.`;
+    console.log(postData);
+    fetch('/contactus', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(postData)
+            })
+          	.then(response => response.json())
+            .then(data => console.log(`Email has been scheduled`, data))
+            .catch((error) => console.error(error))
   }
 
   handleMessageSubmit(){
-    const body = `Contact Form Submission from: ${this.props.name} at ${this.props.email}. They said: ${this.props.message}.`;
-    const username = process.env.MC_USER;
-    const password = process.env.MC_PASS;
-    console.log(body, username, password);
+    if (this.props.isRobot){
+      this.props.emailSendFailure("Sorry, robots not allowed.");
+    } else if (!this.props.isHuman) {
+      this.props.emailSendFailure("Sorry, humans only.");
+    } else if (!this.props.email) {
+      this.props.emailSendFailure("Sorry, email address required.");
+    } else if (!this.props.name) {
+      this.props.emailSendFailure("Sorry, name is required.");
+    } else {
+      this.sendEmail();
+    }
   }
 
   render() {
     return (
       <div>
           <h2>Contact Us</h2>
-          {this.props.errorMessage ? <p>this.props.errorMessage</p> : null}
+          {this.props.errorMessage ? <p className="error">{this.props.errorMessage}</p> : null}
           <form>
             <div className="field">
               <input
@@ -27,6 +52,7 @@ class Form extends React.Component {
                 ref='name'
                 placeholder="Name"
                 value={this.props.name}
+                required
                 onChange={e => this.props.updateName(e.target.value)}/>
             </div>
             <div className="field">
@@ -35,10 +61,12 @@ class Form extends React.Component {
                 ref="email"
                 placeholder="Email"
                 value={this.props.email}
+                required
                 onChange={e => this.props.updateEmail(e.target.value)}/>
             </div>
             <div className="field">
               <textarea
+                type="text"
                 placeholder="Message"
                 ref="message"
                 rows="4"
@@ -50,14 +78,23 @@ class Form extends React.Component {
               <input
                 type="checkbox"
                 checked={this.props.isHuman}
-                // onChange={e => this.props.updateIsHuman("isHuman", !this.props.isHuman)}
               />
-              <label onClick={console.log("click!")}>I'm a human</label>
+              <label onClick={e => this.props.updateIsHuman(!this.props.isHuman)}>I'm a human</label>
             </div>
             <div className="field">
               <label>But are you a robot?</label>
-              <input type="radio" id="robot_yes" name="robot"/><label>Yes</label>
-              <input type="radio" id="robot_no" name="robot" /><label>No</label>
+              <input
+                type="radio"
+                id="robot_yes"
+                name="robot_yes"
+                checked={this.props.isRobot} />
+              <label onClick={e => this.props.updateIsRobot(true)}>Yes</label>
+              <input
+                type="radio"
+                id="robot_no"
+                name="robot_no"
+                checked={!this.props.isRobot} />
+              <label onClick={e => this.props.updateIsRobot(false)}>No</label>
             </div>
             <ul className="actions">
               <li><a className="button" onClick={this.handleMessageSubmit}>Get Started</a></li>
