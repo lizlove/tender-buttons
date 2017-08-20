bf21382a8a720d341e5a295b7aeb89f4ddf5691d
lizlove <elizabethlovero@gmail.com>
2017-08-20 15:15:07 -0400

    Copy stub with neutrino build.

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
-import App from './components/App.js';
-
-ReactDOM.render(<App history={browserHistory}/>, document.getElementById('root'));
